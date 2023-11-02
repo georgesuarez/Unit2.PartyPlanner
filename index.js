ff4dba7c4ceeea34d5ae57517bdde7b913375c62
George Suarez <georgejsuarez@gmail.com>
2023-11-02 12:25:48 -0700

    Finished the workshop

--- index.js
+++ index.js
@@ -36,8 +36,17 @@ async function renderEvents() {
         const li = document.createElement("li");
         li.innerHTML = `
             <h2>Name: ${event.name}</h2>
+            <p>Location: ${event.location}</p>
+            <p>Date: ${event.date}</p>
             <p>Description: ${event.description}</p>
         `;
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", async () => {
+            await deleteEvent(event.id);
+            getEvents();
+        }); 
+        li.append(deleteButton);
         return li;
     });
     eventsList.replaceChildren(...eventsCards);
@@ -49,19 +58,33 @@ async function addEvent(event) {
     try {
         const response = await fetch(API_URL, {
             method: "POST",
-            headers: { "Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                name: addEventForm.name.value,
-                description: addEventForm.description.value
+                name: event.target.name.value,
+                location: event.target.location.value,
+                date: event.target.date.value + ":00.000Z",
+                description: event.target.description.value,
             }),
         });
+        const result = await response.json();
+        console.log(result);
 
         if (!response.ok) {
             throw new Error("Failed to create new event");
         }
-        
-        render();
 
+        render();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deleteEvent(id) {
+    try {
+        await fetch(`${API_URL}/${id}`, {
+            method: "DELETE",
+        });
+        render();
     } catch (error) {
         console.log(error);
     }
